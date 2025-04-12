b5f4b41444a75a86f71774d1b263f06d5e302fbe
Melek <159698535+LukaTN@users.noreply.github.com>
2025-04-12 15:34:37 +0100

    Update training-session.component.ts

--- src/app/front-office/pages/training-session/training-session.component.ts
+++ src/app/front-office/pages/training-session/training-session.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { TrainingSessionService } from '../../../back-office/TrainingSessionMangment/services/TrainingSession.service';
 @Component({
   selector: 'app-training-session',
   templateUrl: './training-session.component.html',
   styleUrls: ['./training-session.component.css']
 })
-export class TrainingSessionComponent {
-  
+export class TrainingSessionComponent implements OnInit {
+  trainingSessions: any[] = [];
+
+
+  constructor(private trainingSessionService: TrainingSessionService) {}
+
+  ngOnInit(): void {
+    this.loadTrainingSessions();
+    console.log('Training sessions loaded:', this.trainingSessions);
+  }
+
+  loadTrainingSessions(): void {
+    this.trainingSessionService.getTrainingSessions()
+      .subscribe({
+        next: (data) => this.trainingSessions = data,
+        error: (error) => console.error('Error loading training sessions:', error)
+      });
+  }
+
 
 }
+
+
