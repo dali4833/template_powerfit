132523117d688dc30d45d37248f7430b45949f19
LukaTN <melek.messaoudi@esprit.tn>
2025-04-27 12:25:50 +0100

    update

--- src/app/back-office/ReviewManagment/editadd/editadd.component.ts
+++ src/app/back-office/ReviewManagment/editadd/editadd.component.ts
@@ -13,7 +13,7 @@ export class EditaddComponent implements OnInit {
   loading = false;
   errorMessage = '';
   reviewId: number | null = null;
-  sessionId = 6; 
+  sessionId = 6;
 
   constructor(
     private fb: FormBuilder,
